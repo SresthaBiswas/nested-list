3915ebc6b16c04310eb0f43593c310975526d744
SresthaBiswas <106442035+SresthaBiswas@users.noreply.github.com>
2022-07-15 10:20:14 +0530

    Add files via upload

---
+++ pdf48.py
@@ -0,0 +1,18 @@
+# Write a Python program to split a given list into specified sized chunks.
+# Original list:
+# [12, 45, 23, 67, 78, 90, 45, 32, 100, 76, 38, 62, 73, 29, 83]
+# Split the said list into equal size 3
+# [[12, 45, 23], [67, 78, 90], [45, 32, 100], [76, 38, 62], [73, 29, 83]]
+# Split the said list into equal size 4
+# [[12, 45, 23, 67], [78, 90, 45, 32], [100, 76, 38, 62], [73, 29, 83]]
+# Split the said list into equal size 5
+# [[12, 45, 23, 67, 78], [90, 45, 32, 100, 76], [38, 62, 73, 29, 83]]
+list=[12, 45, 23, 67, 78, 90, 45, 32, 100, 76, 38, 62, 73, 29, 83]
+i=0
+l=[]
+split=int(input("enter specific size"))
+while i<len(list):
+    a=(list[i:i+split])
+    l.append(a)
+    i=i+split
+print(l)
